c04b61226092639a5d71d62c2b2c84aa7c11fe6d
Brian Tomlinson <brian@smartm.no>
2013-11-04 15:27:24 +0100

    Fleshed out a bit more of the skeleton.  Placeholder logic, nothing to see here until we get some direction.

--- server.py
+++ server.py
@@ -1,20 +1,68 @@
-from pprint import pprint
-
 import webapp2
 from paste import httpserver
 
 __author__ = "Brian Tomlinson <darthlukan@gmail.com>"
 
 
+class Processor(object):
+    """
+    Performs some basic validation of data and cleanup as needed
+    before passing off to the "heavy lifter"
+
+    NOTE: Dummy logic for now as placeholders.
+    """
+    def __init__(self):
+        pass
+
+    def validate(self, params):
+        """
+        Do we even have valid data to work with?
+        """
+        if params:
+            return "validated: %s" % params
+        return False
+
+    def clean(self, params):
+        """
+        Clean it up prior to sending to the processor
+        """
+        if params:
+            return "cleaned: %s" % params
+        return False
+
+    def process(self, params):
+        """
+        Send to processor and return whatever we get back.
+        """
+        params = "processor_module_call_here(%s)" % params
+        return params
+
+    def validation_entry(self, params):
+        validated = self.validate(params)
+        cleaned = self.clean(validated)
+        processed = self.process(cleaned)
+        return processed
+
+
 class Handler(webapp2.RequestHandler):
 
     def get(self):
-        pprint(self.request.GET)
-        self.response.write('GET OK')
+        proc = Processor()
+        params = self.request.GET
+        processed = proc.validation_entry(params)
+        if processed:
+            self.response.write("validated, cleaned, and processed GET data: %s" % processed)
+        else:
+            self.response.write('Error handling and appropriate status code would go here')
 
     def post(self):
-        pprint(self.request.POST)
-        self.response.write('POST OK')
+        proc = Processor()
+        params = self.request.POST
+        processed = proc.validation_entry(params)
+        if processed:
+            self.response.write("validated, cleaned, and processed POST data: %s" % processed)
+        else:
+            self.response.write('Error handling and appropriate status code goes here.')
 
 
 if __name__ == '__main__':
