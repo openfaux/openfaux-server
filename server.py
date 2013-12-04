20a2f7ff8312d1693414bafe86b4838c7b3372ef
Yashin Mehaboobe <yashinm92@gmail.com>
2013-12-04 10:55:44 +0530

    Created server.py with twisted framework, Implemented proxy.

---
+++ server.py
@@ -0,0 +1,54 @@
+from twisted.python import log
+from twisted.web import http, proxy
+
+__author__ = "Yashin Mehaboobe aka sp3ctr3"
+
+class ProxyClient(proxy.ProxyClient):
+    """Modify response as well as header here.
+    """
+    def handleHeader(self, key, value):
+        """
+        Modify header here
+        """
+        log.msg("Header: %s: %s" % (key, value))
+        proxy.ProxyClient.handleHeader(self, key, value)
+
+    def handleResponsePart(self, buffer):
+        """
+        Modify buffer to modify response. For example replacing buffer with buffer[::-1] will lead to a reversed output.
+        This might cause content encoding errors. Currently test only on text only websites
+        """
+        log.msg("Content: %s" % (buffer,))
+        proxy.ProxyClient.handleResponsePart(self, buffer)
+
+class ProxyClientFactory(proxy.ProxyClientFactory):
+    protocol = ProxyClient
+
+class ProxyRequest(proxy.ProxyRequest):
+    protocols = dict(http=ProxyClientFactory)
+
+class Proxy(proxy.Proxy):
+    requestFactory = ProxyRequest
+
+class ProxyFactory(http.HTTPFactory):
+    protocol = Proxy
+portstr = "tcp:8080:interface=localhost" # serve on localhost:8080
+
+if __name__ == '__main__': 
+    import sys
+    from twisted.internet import endpoints, reactor
+
+    def shutdown(reason, reactor, stopping=[]):
+        """Stop the reactor."""
+        if stopping: return
+        stopping.append(True)
+        if reason:
+            log.msg(reason.value)
+        reactor.callWhenRunning(reactor.stop)
+
+    log.startLogging(sys.stdout)
+    endpoint = endpoints.serverFromString(reactor, portstr)
+    d = endpoint.listen(ProxyFactory())
+    d.addErrback(shutdown, reactor)
+    reactor.run()
+
