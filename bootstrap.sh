487c1ce642229d6767b16320fad548ecc0b83ebb
Zlatan Vasović <legospace9876@gmail.com>
2014-01-17 20:52:09 +0000

    Remove 'oh's

--- bootstrap.sh
+++ bootstrap.sh
@@ -6,21 +6,21 @@ set -e
 
 if ! which python > /dev/null
 then
-  echo -e "\nOh. You don't have Python."
+  echo -e "\nYou don't have Python."
   echo "Installing Python..."
   sudo apt-get install python -y > /tmp/python-install.log
 fi
 
 if ! which pip > /dev/null
 then
-  echo -e "\nOh. You don't have Pip."
+  echo -e "\nYou don't have Pip."
   echo "Installing Pip..."
   sudo apt-get install python-pip -y > /tmp/pip-install.log
 fi
 
 if ! python -c "import twisted" 2> /dev/null
 then
-  echo -e "\nOh. You don't have Twisted."
+  echo -e "\nYou don't have Twisted."
   echo "Installing Twisted..."
   sudo pip install python-twisted>=13.2.0 > /tmp/twisted-install.log
 fi
